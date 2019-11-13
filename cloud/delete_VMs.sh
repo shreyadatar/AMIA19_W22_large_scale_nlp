df9e850084640b142a629c2fb11977ac2392d9c4
Raymond Finzel <prometheoid@gmail.com>
2019-11-13 17:04:28 -0600

    Revert "utility scripts for dealing with VM management on DigitalOcean"

--- cloud/delete_VMs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-auth=$(cat auth)
-tag="amiaW22"
-
-curl -sS -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer ${auth}" "https://api.digitalocean.com/v2/droplets?tag_name=$tag"
